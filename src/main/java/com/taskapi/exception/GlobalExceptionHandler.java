ac7f1d1354cdefde592760192ade2dca1f0a18fb
art <arthurevaristo1@gmail.com>
2026-09-07 22:54:54 -0300

    feat: implementa Exceptions (in progress).

---
+++ src/main/java/com/taskapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.taskapi.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleValidationExceptions(Exception e) {
+
+    }
+}
---
+++ src/main/java/com/taskapi/exception/ValidationErrorResponse.java
@@ -0,0 +1,39 @@
+package com.taskapi.exception;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class ValidationErrorResponse {
+    private LocalDateTime dataHora = LocalDateTime.now();
+    private int status;
+    private Map<String, String> errors;
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public ValidationErrorResponse(int status, Map<String, String> errors) {
+        this.status = status;
+        this.errors = errors;
+    }
+}
